f5d8e469c5ad1db77ec5595339a20151b3b86bde
Фаррух Умурзаков <ya.on-line@bk.ru>
2018-10-23 01:59:28 +0500

    13 den' extend ne rabotaet 2

---
+++ cache/backend/dev/modules/autoJob/templates/_list_actions.php
@@ -0,0 +1,3 @@
+<li class="sf_admin_action_deleteneveractivated">
+  <?php echo link_to(__('Delete never activated jobs', array(), 'messages'), 'Job/ListDeleteNeverActivated', array()) ?>
+</li>
---
+++ cache/backend/dev/modules/autoJob/templates/_list_th_stacked.php
@@ -0,0 +1,1 @@
+<?php include_partial('Job/list_th_tabular', array('sort' => $sort)) ?>
